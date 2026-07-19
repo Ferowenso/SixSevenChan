9e80a4406812d81efaa02f7680205e88b3ca4740
ferowenso <ferowenso@yandex.ru>
2026-07-19 15:59:57 +0400

    Initial commit

---
+++ src/main/java/com/example/sixsevenchan/SixSevenChanApplication.java
@@ -0,0 +1,13 @@
+package com.example.sixsevenchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixSevenChanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixSevenChanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sixsevenchan/SixSevenChanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sixsevenchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixSevenChanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
